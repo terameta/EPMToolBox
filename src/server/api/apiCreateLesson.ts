932d5aaa49c59c1d78b0cb490a920a5bed974a3b
alirizadikici <aliriza.dikici@gmail.com>
2017-04-16 12:21:13 +0400

    API Complete

--- src/server/api/apiCreateLesson.ts
+++ src/server/api/apiCreateLesson.ts
@@ -3,9 +3,11 @@ import * as _ from 'lodash';
 import { createLesson } from '../queries/createLesson';
 import { onSuccess } from './onSuccess';
 import { onError } from './onError';
+import { databaseErrorHandler } from './databaseErrorHandler';
 
 export function apiCreateLesson(req: Request, res: Response) {
 	createLesson(req.body).
 	then( _.partial(onSuccess, res) ).
+	catch( _.partial(databaseErrorHandler, res)).
 	catch( _.partial(onError, res, 'Could not create lesson') );
 }
