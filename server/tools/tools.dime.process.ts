4351406ead71f16a9eb028d4757859fa28ecf50d
AliRiza.Dikici <ARizaDikici@kzlnet.com>
2017-08-15 10:59:31 +0400

    Removed IFNULL from group_concat query, because  we are already handling this edge case on the pushdata action as a necessary step. No need to repeat.

--- server/tools/tools.dime.process.ts
+++ server/tools/tools.dime.process.ts
@@ -1665,7 +1665,7 @@ export class ProcessTools {
 							let concaters: string[]; concaters = [];
 							rows.forEach(( curTuple: any ) => {
 								if ( curTuple[denseField] !== 'ignore' && curTuple[denseField] !== 'ignore:ignore' && curTuple[denseField] !== 'ignore::ignore' && curTuple[denseField] !== 'missing' ) {
-									concaters.push( 'IFNULL(GROUP_CONCAT((CASE ' + denseField + ' WHEN \'' + curTuple[denseField] + '\' THEN SUMMARIZEDRESULT ELSE NULL END)), \'#missing\') AS \'' + curTuple[denseField] + '\'' );
+									concaters.push( 'GROUP_CONCAT((CASE ' + denseField + ' WHEN \'' + curTuple[denseField] + '\' THEN SUMMARIZEDRESULT ELSE NULL END)) AS \'' + curTuple[denseField] + '\'' );
 								}
 							} );
 							// console.log( selecters );
