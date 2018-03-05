c7a6577bf77434f1dd8abdc4ab28d2b64c255c22
AliRiza.Dikici <ARizaDikici@kzlnet.com>
2018-03-05 15:08:55 +0400

    Stream Type correction

--- shared/enums/dime/streamtypes.ts
+++ shared/enums/dime/streamtypes.ts
@@ -1,17 +1,17 @@
 export enum DimeStreamType {
-	'RDBT' = 1,
-	'HPDB' = 2
+	'RDBT' = 2,
+	'HPDB' = 1
 }
 
 export function dimeGetStreamTypeDescription( typecode: number | string ) {
 	switch ( typecode ) {
-		case 1:
-		case '1':
+		case 2:
+		case '2':
 		case 'RDBT': {
 			return 'Relational Database Table/View';
 		}
-		case 2:
-		case '2':
+		case 1:
+		case '1':
 		case 'HPDB': {
 			return 'Hyperion Planning Database';
 		}
