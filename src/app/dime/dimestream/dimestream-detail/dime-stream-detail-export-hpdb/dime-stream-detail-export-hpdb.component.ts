60ecfe6b746c538bed9f66c42dec30d4c6a3a1da
AliRiza.Dikici <ARizaDikici@kzlnet.com>
2018-03-20 17:55:08 +0400

    Stream Export in progress

--- src/app/dime/dimestream/dimestream-detail/dime-stream-detail-export-hpdb/dime-stream-detail-export-hpdb.component.ts
+++ src/app/dime/dimestream/dimestream-detail/dime-stream-detail-export-hpdb/dime-stream-detail-export-hpdb.component.ts
@@ -9,12 +9,41 @@ import { DimeStreamService } from '../../dimestream.service';
 export class DimeStreamDetailExportHPDBComponent implements OnInit {
 	public rowDims: any[] = [];
 	public colDims: any[] = [];
+	public povDims: any[] = [];
 
 	constructor(
 		public mainService: DimeStreamService
 	) { }
 
 	ngOnInit() {
+	}
+
+	private waitUntilReady = () => {
+
+	}
+
+	public isAssigned = ( fieldName: string ) => {
+		let assigned = false;
+		this.rowDims.filter( row => row.name === fieldName ).forEach( row => {
+			assigned = true;
+		} );
+		this.colDims.filter( row => row.name === fieldName ).forEach( row => {
+			assigned = true;
+		} );
+		this.povDims.filter( row => row.name === fieldName ).forEach( row => {
+			assigned = true;
+		} );
+		return assigned;
+	}
+
+	public addToRows = ( fieldName: string ) => {
+		this.rowDims.push( { name: fieldName } );
+	}
+	public addToCols = ( fieldName: string ) => {
+		this.colDims.push( { name: fieldName } );
+	}
+	public addToPOVs = ( fieldName: string ) => {
+		this.povDims.push( { name: fieldName } );
 	}
 
 }
