a5328b04e22ef7a30bd4d8541d227847def5cea4
AliRiza.Dikici <ARizaDikici@kzlnet.com>
2018-04-08 17:59:49 +0400

    We are now back at Defining Exports

--- src/app/dime/dimestream/dimestream-detail/dime-stream-detail-export-detail/dime-stream-detail-export-detail.component.ts
+++ src/app/dime/dimestream/dimestream-detail/dime-stream-detail-export-detail/dime-stream-detail-export-detail.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DimeStreamService } from '../../dimestream.service';
+import { DimeStreamType } from '../../../../../../shared/enums/dime/streamtypes';
 
-@Component({
-  selector: 'app-dime-stream-detail-export-detail',
-  templateUrl: './dime-stream-detail-export-detail.component.html',
-  styleUrls: ['./dime-stream-detail-export-detail.component.scss']
-})
-export class DimeStreamDetailExportDetailComponent implements OnInit {
+@Component( {
+	selector: 'app-dime-stream-detail-export-detail',
+	templateUrl: './dime-stream-detail-export-detail.component.html',
+	styleUrls: ['./dime-stream-detail-export-detail.component.scss']
+} )
+export class DimeStreamDetailExportDetailComponent implements OnInit, OnDestroy {
+	private streamTypes = DimeStreamType;
+	public export: any = {};
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+	constructor(
+		public mainService: DimeStreamService
+	) { }
+
+	ngOnInit() { }
+
+	ngOnDestroy() { }
 
 }
